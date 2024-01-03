ae78a6bf95a65c8a423206e13bc19bf27563d070
Kalène Apiou <apiou.kalene@gmail.com>
2024-01-03 07:42:17 +0100

    up

--- components/RecipeList.js
+++ components/RecipeList.js
@@ -2,6 +2,7 @@
 import jsxParser from '../utils/jsxParser.js';
 
 export function RecipeListComponent(recipes) {
+    console.log(recipes);
     const recipeItems = recipes.map(recipe => 
         `<div class="recipe">${recipe.name}</div>`
     ).join('');
