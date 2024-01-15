3c30fd14a09c978102a0674ab0c46b6bee8e6ac3
Kalène Apiou <apiou.kalene@gmail.com>
2024-01-15 19:07:25 +0100

    add cards

--- components/RecipeList.js
+++ components/RecipeList.js
@@ -1,5 +1,6 @@
 // components/RecipeListComponent.js
 import jsxParser from "../utils/jsxParser.js";
+import limiteDescription from "../utils/limiteDescription.js";
 
 export function RecipeListComponent(recipes) {
     console.log(recipes);
@@ -8,7 +9,9 @@ export function RecipeListComponent(recipes) {
             (recipe) => /*html*/ `
             <div class="recipe-container">
                 <div class="recipe-container__image">
-                    <img src="./../assets/Les_petits_plats/${recipe.image}" alt="${recipe.name}" />
+                    <img src="./../assets/Les_petits_plats/${
+                        recipe.image
+                    }" alt="${recipe.name}" />
                 </div>
                 <div class="recipe-container__content">
                     <div class="recipe-container__content__time">
@@ -17,6 +20,26 @@ export function RecipeListComponent(recipes) {
                     <div class="recipe-container__content__title">
                         <h2>${recipe.name}</h2>
                     </div>
+                    <div class="recipe-container__content__description">
+                        <span>RECETTE</span>
+                        <p>${limiteDescription(recipe.description)}</p>
+                   </div>
+                    <div class="recipe-container__content__ingredients">
+                        <span>INGRÉDIENTS</span>
+                        <ul class="recipe-container__content__ingredients__quantity">
+                            ${recipe.ingredients
+                                .map((ingredient) => {
+                                    return /*html*/ `
+                                            <ul >
+                                                <li class="ingredient"> ${ingredient.ingredient} </li>
+                                                <li class="quantityUnit"> ${ingredient.quantity ? ingredient.quantity : ""} ${ingredient.unit ? ingredient.unit : ""} </li>
+                                            </ul>
+                                `;
+                                })
+                                .join("")}
+                        </ul>
+                    </div>
+
                 </div>
             </div>
         `
---
+++ utils/limiteDescription.js
@@ -0,0 +1,10 @@
+// limiter le nombres de caractere dans la description fait une fonction 
+
+function limiteDescription(description) { 
+    if (description.length > 150) {
+        return description.slice(0, 150) + "...";
+    }
+    return description;
+}
+ 
+export default limiteDescription;
